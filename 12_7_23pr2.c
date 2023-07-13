3f5742a5031ae9562bbba5dc383d1a882eab98ec
imvishalgirase <137286597+imvishalgirase@users.noreply.github.com>
2023-07-13 17:17:30 +0530

    Add files via upload

---
+++ 12_7_23pr2.c
@@ -0,0 +1,22 @@
+// C Program to calculate size of an array using sizeof()
+// operator
+#include <stdio.h>
+
+int main()
+{
+
+	int a[] = { 1, 2, 3, 4, 5 };
+	int i;
+	for(i=0;i<=5;i++)
+	{
+	}
+	{
+		
+
+
+	printf("The length of the array is: %d\n", i);
+}
+
+	
+}
+
---
+++ 12_7_23pr3.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+main()
+{
+	   int i,n,sum=0,avg;
+	  int a[10];
+	   printf("enter the total number of array=");
+	   scanf("%d",&n);
+	   printf("Enter elemnt element=");
+	   for(i=0;i<n;i++)
+	   {
+	   printf("a[%d]=",i);
+	   scanf("%d",&a[i]);
+}
+      	 for(i=0;i<n;i++)
+      	 {
+      	 	
+      	 	sum=sum+a[i];
+      	 
+		   }
+		      printf("sum of array=%d\n",sum);
+		      avg=sum/n;
+             printf("The average of sum is=%d",avg);
+         }
+
+     
+	   	  
+	   	  
+	
+	   
+	   
+	   
+	   
+	   
+	   
+		
+	
+	
+	
+
