7cf3d907b51389cdd837799dd3dbff386c996700
imvishalgirase <137286597+imvishalgirase@users.noreply.github.com>
2023-07-17 16:43:50 +0530

    Add files via upload

---
+++ 12_7_23pr1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+	int n,i;
+	int a[10];
+	printf("Plz enter the total number of array element=");
+	scanf("%d",&n);
+	printf("Plz enter the array elements=");
+	
+	for(i<0;i<=n;i++)
+	{
+		printf("a[%d]= ",i);
+		scanf("%d",&a[i]);
+			}
+				for(i=0;i<=n;i++)	
+				{
+					printf("%d \n",a[i]);
+					
+					 
+					
+				}
+	
+	
+	
+	
+}
